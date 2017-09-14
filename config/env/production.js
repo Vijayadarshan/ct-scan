200e0a71b81f9da596c9dc2d3a2982f8739c9198
admin-PC\admin <nimish_chandola@yahoo.com>
2017-09-15 01:16:46 +0530

    Added mlab path

--- config/env/production.js
+++ config/env/production.js
@@ -4,7 +4,7 @@ module.exports = {
   secure: true,
   port: process.env.PORT || 8080,
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://vijay:pr0nt0net@ds135364.mlab.com:35364/ctscan',
     options: {
       user: '',
       pass: ''
